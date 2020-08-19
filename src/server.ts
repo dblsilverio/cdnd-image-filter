4615dc9aa562cf69acfeb3b2cb91de8ceaddc35b
Diogo Silvério <dblsilverio+github@gmail.com>
2020-08-18 22:11:11 -0300

    Basic exception handling

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles, validateImage} from './util/util';
+import { filterImageFromURL, deleteLocalFiles, validateImage } from './util/util';
 
 (async () => {
 
@@ -9,19 +9,12 @@ import {filterImageFromURL, deleteLocalFiles, validateImage} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
@@ -31,27 +24,36 @@ import {filterImageFromURL, deleteLocalFiles, validateImage} from './util/util';
 
   app.get("/filteredimage", [validateImage], async (req: Request, res: Response) => {
 
-    const {imageUrl} = req.body;
+    const { imageUrl } = req.body;
 
-    const filteredImage = await filterImageFromURL(imageUrl);
+    try {
+      const filteredImage = await filterImageFromURL(imageUrl);
+      res.sendFile(filteredImage, (err) => {
+        deleteLocalFiles([filteredImage]);
 
-    res.sendFile(filteredImage, (err) => {
-      deleteLocalFiles([filteredImage]);
-    });
+        if (err) {
+          console.error(`Error sending file: ${err}`);
+          res.send({ error: err }).status(500);
+        }
+      });
+    } catch (exc) {
+      console.error(`Exception filtering image: ${exc}`);
+      res.send({ error: `${exc}` }).status(500);
+    }
 
   });
 
-  //! END @TODO1
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req: Request, res: Response ) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{URL}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
